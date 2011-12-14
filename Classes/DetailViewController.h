88a0005b6ab580cb26f2574af6cb629d2eb03599
Adam Oliner <oliner@gmail.com>
2011-12-14 21:59:13 +0000

    someday, this code will be presentable

--- Classes/DetailViewController.h
+++ Classes/DetailViewController.h
@@ -16,10 +16,31 @@
     MBProgressHUD *HUD;
     
     NSString *navTitle;
+    
+    NSArray *detailGraphView;
+    NSArray *appName;
+    NSArray *appIcon;
+    NSArray *appScore;
+    NSArray *numSamplesWith;
+    NSArray *numSamplesWithout;
+    NSArray *wassersteinDistance;
+    IBOutlet UIView *portraitView;
+	IBOutlet UIView *landscapeView;
 }
 
 @property (assign, nonatomic) BOOL firstAppearance;
 @property (assign, nonatomic) NSString *navTitle;
+
+@property (retain, nonatomic) IBOutletCollection(CPTGraphHostingView) NSArray *detailGraphView;
+@property (retain, nonatomic) IBOutletCollection(UILabel) NSArray *appName;
+@property (retain, nonatomic) IBOutletCollection(UIImageView) NSArray *appIcon;
+@property (retain, nonatomic) IBOutletCollection(UIProgressView) NSArray *appScore;
+@property (retain, nonatomic) IBOutletCollection(UILabel) NSArray *numSamplesWith;
+@property (retain, nonatomic) IBOutletCollection(UILabel) NSArray *numSamplesWithout;
+@property (retain, nonatomic) IBOutletCollection(UILabel) NSArray *wassersteinDistance;
+
+@property (nonatomic, retain) IBOutlet UIView *portraitView;
+@property (nonatomic, retain) IBOutlet UIView *landscapeView;
 
 - (void)loadDetailDataWithHUD;
 - (BOOL)isFresh;
--- Classes/HogDetailViewController.h
+++ Classes/HogDetailViewController.h
@@ -11,27 +11,5 @@
 #import "DetailViewController.h"
 
 @interface HogDetailViewController : DetailViewController
-{
-    NSArray *detailGraphView;
-    NSArray *appName;
-    NSArray *appIcon;
-    NSArray *appScore;
-    NSArray *numSamplesWith;
-    NSArray *numSamplesWithout;
-    NSArray *wassersteinDistance;
-    IBOutlet UIView *portraitView;
-	IBOutlet UIView *landscapeView;
-}
-
-@property (retain, nonatomic) IBOutletCollection(CPTGraphHostingView) NSArray *detailGraphView;
-@property (retain, nonatomic) IBOutletCollection(UILabel) NSArray *appName;
-@property (retain, nonatomic) IBOutletCollection(UIImageView) NSArray *appIcon;
-@property (retain, nonatomic) IBOutletCollection(UIProgressView) NSArray *appScore;
-@property (retain, nonatomic) IBOutletCollection(UILabel) NSArray *numSamplesWith;
-@property (retain, nonatomic) IBOutletCollection(UILabel) NSArray *numSamplesWithout;
-@property (retain, nonatomic) IBOutletCollection(UILabel) NSArray *wassersteinDistance;
-
-@property (nonatomic, retain) IBOutlet UIView *portraitView;
-@property (nonatomic, retain) IBOutlet UIView *landscapeView;
 
 @end
