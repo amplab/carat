1e4dd60ee052d6c3d36eead030a31c47b622764c
Adam Oliner <oliner@gmail.com>
2012-01-17 23:56:34 +0000

    moved for easier customization and faster loading

--- Classes/AboutViewController.m
+++ Classes/AboutViewController.m
@@ -19,6 +19,19 @@
 	if (self) {
         self.title = @"About";
         self.tabBarItem.image = [UIImage imageNamed:@"about"];
+        self.theHTML = @"<html> \
+        <head> \
+        </head> \
+        <body> \
+        <h3>Carat: collaborative detection of energy bugs</h3> \
+        <p>Carat is a research project from the AMP Lab at UC Berkeley that aims to perform collaborative detection of energy bugs.<p> \
+        <p><b><i>Bug</i></b> means an app that is using a lot of energy on a small number of devices, including yours. Restarting a buggy app may improve battery life.<p> \
+        <p><b><i>Hog</i></b> means an app that is correlated with higher energy use, across many devices. Closing this app may improve battery life.<p> \
+        <p>Carat collects and reports generic usage statistics about your phone that allow it to do its job; this process involves absolutely no personally identifying information. Period.<p> \
+        <p>Carat is under development, so please check back often to see new data and new analyses. The more you use it, the better it will get. You can read more about the research on the <a href=\"http://carat.cs.bereley.edu\">Carat Project page</a>.<p> \
+        <p>You can also <a href=\"mailto:oliner+carat@eecs.berkeley.edu\">contact us directly</a>.<p> \
+        </body> \
+        </html>";
     }
     return self;
 }
@@ -58,19 +71,6 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    self.theHTML = @"<html> \
-    <head> \
-    </head> \
-    <body> \
-    <h3>Carat: collaborative detection of energy bugs</h3> \
-    <p>Carat is a research project from the AMP Lab at UC Berkeley that aims to perform collaborative detection of energy bugs.<p> \
-    <p><b><i>Bug</i></b> means an app that is using a lot of energy on a small number of devices, including yours. Restarting a buggy app may improve battery life.<p> \
-    <p><b><i>Hog</i></b> means an app that is correlated with higher energy use, across many devices. Closing this app may improve battery life.<p> \
-    <p>Carat collects and reports generic usage statistics about your phone that allow it to do its job; this process involves absolutely no personally identifying information. Period.<p> \
-    <p>Carat is under development, so please check back often to see new data and new analyses. The more you use it, the better it will get. You can read more about the research on the <a href=\"http://carat.cs.bereley.edu\">Carat Project page</a>.<p> \
-    <p>You can also <a href=\"mailto:oliner+carat@eecs.berkeley.edu\">contact us directly</a>.<p> \
-    </body> \
-    </html>";
     for (UIWebView *wv in self.aboutWebView) {
         [wv loadHTMLString:self.theHTML baseURL:nil];
     }
