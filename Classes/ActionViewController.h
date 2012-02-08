174f83e5c613e00e0136400c2a633a1e48746201
Adam Oliner <oliner@gmail.com>
2012-02-08 00:26:37 +0000

    ActionView

---
+++ Classes/ActionViewController.h
@@ -0,0 +1,17 @@
+//
+//  ActionViewController.h
+//  Carat
+//
+//  Created by Adam Oliner on 2/7/12.
+//  Copyright (c) 2012 UC Berkeley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActionViewController : UIViewController {
+    IBOutlet UIView *dataTable;
+}
+
+@property (retain, nonatomic) IBOutlet UIView *dataTable;
+
+@end
