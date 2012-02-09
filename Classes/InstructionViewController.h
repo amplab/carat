8620b66c5c76715512e3ae40a14de9497a518eb9
Adam Oliner <oliner@gmail.com>
2012-02-09 19:59:07 +0000

    adding files for the instruction view

---
+++ Classes/InstructionViewController.h
@@ -0,0 +1,13 @@
+//
+//  InstructionViewController.h
+//  Carat
+//
+//  Created by Adam Oliner on 2/9/12.
+//  Copyright (c) 2012 UC Berkeley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InstructionViewController : UIViewController
+
+@end
---
+++ Classes/InstructionViewController.m
@@ -0,0 +1,90 @@
+//
+//  InstructionViewController.m
+//  Carat
+//
+//  Created by Adam Oliner on 2/9/12.
+//  Copyright (c) 2012 UC Berkeley. All rights reserved.
+//
+
+#import "InstructionViewController.h"
+#import "Sampler.h"
+
+@implementation InstructionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - UIWebView
+
+- (BOOL)webView:(UIWebView *)inWeb shouldStartLoadWithRequest:(NSURLRequest *)inRequest navigationType:(UIWebViewNavigationType)inType {
+    if ( inType == UIWebViewNavigationTypeLinkClicked ) {
+        [[UIApplication sharedApplication] openURL:[inRequest URL]];
+        return NO;
+    }
+    
+    return YES;
+}
+
+- (void)webView:(UIWebView *)wv didFailLoadWithError:(NSError *)error {
+    // Ignore NSURLErrorDomain error -999.
+    if (error.code == NSURLErrorCancelled) return;
+    
+    // Ignore "Fame Load Interrupted" errors. Seen after app store links.
+    if (error.code == 102 && [error.domain isEqual:@"WebKitErrorDomain"]) return;
+    
+    // Normal error handling…
+}
+
+#pragma mark - View lifecycle
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView
+{
+}
+*/
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+ 
+    self.navigationItem.title = @"Action Instructions";
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [[Sampler instance] checkConnectivityAndSendStoredDataToServer];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return YES;
+}
+
+
+@end
