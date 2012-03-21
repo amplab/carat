1b16f683dc74294e5a9d511db70c58dc57c7029c
Adam Oliner <oliner@gmail.com>
2012-03-21 09:57:20 -0700

    created ConsentView

---
+++ app/ios/Classes/ConsentViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConsentViewController.h
+//  Carat
+//
+//  Created by Adam Oliner on 3/21/12.
+//  Copyright (c) 2012 UC Berkeley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConsentViewController : UIViewController
+
+@end
