bf4df2d2e424a50268f783b18079322c2492ac7a
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2012-03-28 21:05:07 +0300

    Added a blank Android app

---
+++ app/android/src/edu/berkeley/cs/amplab/carat/android/CaratSuggestionsActivity.java
@@ -0,0 +1,13 @@
+package edu.berkeley.cs.amplab.carat.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CaratSuggestionsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
