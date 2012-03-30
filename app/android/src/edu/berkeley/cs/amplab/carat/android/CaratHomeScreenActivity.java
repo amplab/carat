e4be4c9afc56ce5efd47c41b9247cbe8f5087da8
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2012-03-30 22:01:27 +0300

    Added color strings and a home screen activity.

--- app/android/src/edu/berkeley/cs/amplab/carat/android/CaratHomeScreenActivity.java
+++ app/android/src/edu/berkeley/cs/amplab/carat/android/CaratHomeScreenActivity.java
@@ -3,7 +3,7 @@ package edu.berkeley.cs.amplab.carat.android;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class CaratSuggestionsActivity extends Activity {
+public class CaratHomeScreenActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
