177563f5de20e7965607d02b484d2fd9a7fbbfb2
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2012-04-10 17:29:44 -0700

    Added storage and retrieval of hogs and bugs.

--- app/android/src/edu/berkeley/cs/amplab/carat/android/storage/CaratDataStorage.java
+++ app/android/src/edu/berkeley/cs/amplab/carat/android/storage/CaratDataStorage.java
@@ -8,150 +8,195 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.List;
 
-import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 import android.util.Log;
+import edu.berkeley.cs.amplab.carat.thrift.HogBugReport;
+import edu.berkeley.cs.amplab.carat.thrift.HogsBugs;
 import edu.berkeley.cs.amplab.carat.thrift.Reports;
 
 public class CaratDataStorage {
 
-  public static final String FILENAME = "carat-reports.dat";
-  public static final String FRESHNESS = "carat-freshness.dat";
-  private Application a = null;
-  
-  private long freshness = 0;
-  private Reports caratData = null;
+	public static final String FILENAME = "carat-reports.dat";
+	public static final String BUGFILE = "carat-bugs.dat";
+	public static final String HOGFILE = "carat-hogs.dat";
 
-  public CaratDataStorage(Application a) {
-    this.a = a;
-    freshness = readFreshness();
-    caratData = readReports();
-  }
+	public static final String FRESHNESS = "carat-freshness.dat";
+	private Application a = null;
 
-  public void writeReports(Reports reports) {
-    caratData = reports;
-    writeObject(reports, FILENAME);
-  }
+	private long freshness = 0;
+	private Reports caratData = null;
+	private HogBugReport bugData = null;
+	private HogBugReport hogData = null;
 
-  public void writeFreshness() {
-    freshness = System.currentTimeMillis();
-    writeText(freshness + "", FRESHNESS);
-  }
+	public CaratDataStorage(Application a) {
+		this.a = a;
+		freshness = readFreshness();
+		caratData = readReports();
+	}
 
-  public void writeObject(Object o, String fname) {
-    FileOutputStream fos = getFos(fname);
-    if (fos == null)
-      return;
-    try {
-      ObjectOutputStream dos = new ObjectOutputStream(fos);
-      dos.writeObject(o);
-      dos.close();
-    } catch (IOException e) {
-      Log.e(this.getClass().getName(), "Could not write object:" + o + "!");
-      e.printStackTrace();
-    }
-  }
+	public void writeReports(Reports reports) {
+		caratData = reports;
+		writeObject(reports, FILENAME);
+	}
 
-  public Object readObject(String fname){
-    FileInputStream fin = getFin(fname);
-    if (fin == null)
-      return null;
-    try {
-      ObjectInputStream din = new ObjectInputStream(fin);
-      Object o = din.readObject();
-      din.close();
-      return o;
-    } catch (IOException e) {
-      Log.e(this.getClass().getName(), "Could not read object from " + fname+ "!");
-      e.printStackTrace();
-    } catch (ClassNotFoundException e) {
-      Log.e(this.getClass().getName(), "Could not find class: "+ e.getMessage() +" reading from " + fname+ "!");
-      e.printStackTrace();
-    }
-    return null;
-  }
+	public void writeFreshness() {
+		freshness = System.currentTimeMillis();
+		writeText(freshness + "", FRESHNESS);
+	}
 
-  public void writeText(String thing, String fname) {
-    FileOutputStream fos = getFos(fname);
-    if (fos == null)
-      return;
-    try {
-      DataOutputStream dos = new DataOutputStream(fos);
-      dos.writeUTF(thing);
-      dos.close();
-    } catch (IOException e) {
-      Log.e(this.getClass().getName(), "Could not write text:" + thing + "!");
-      e.printStackTrace();
-    }
-  }
+	public void writeObject(Object o, String fname) {
+		FileOutputStream fos = getFos(fname);
+		if (fos == null)
+			return;
+		try {
+			ObjectOutputStream dos = new ObjectOutputStream(fos);
+			dos.writeObject(o);
+			dos.close();
+		} catch (IOException e) {
+			Log.e(this.getClass().getName(), "Could not write object:" + o
+					+ "!");
+			e.printStackTrace();
+		}
+	}
 
-  public String readText(String fname) {
-    FileInputStream fin = getFin(fname);
-    if (fin == null)
-      return null;
-    try {
-      DataInputStream din = new DataInputStream(fin);
-      String s = din.readUTF();
-      din.close();
-      return s;
-    } catch (IOException e) {
-      Log.e(this.getClass().getName(), "Could not read text from " + fname + "!");
-      e.printStackTrace();
-    }
-    return null;
-  }
-  
-  private FileInputStream getFin(String fname){
-    try {
-      return a.openFileInput(fname);
-    } catch (FileNotFoundException e) {
-      Log.e(this.getClass().getName(), "Could not open carat data file " + fname + " for reading!");
-      e.printStackTrace();
-      return null;
-    }
-  }
-  
-  private FileOutputStream getFos(String fname){
-    try {
-      return a.openFileOutput(fname, Context.MODE_PRIVATE);
-    } catch (FileNotFoundException e) {
-      Log.e(this.getClass().getName(), "Could not open carat data file " + fname + " for writing!");
-      e.printStackTrace();
-      return null;
-    }
-  }
+	public Object readObject(String fname) {
+		FileInputStream fin = getFin(fname);
+		if (fin == null)
+			return null;
+		try {
+			ObjectInputStream din = new ObjectInputStream(fin);
+			Object o = din.readObject();
+			din.close();
+			return o;
+		} catch (IOException e) {
+			Log.e(this.getClass().getName(), "Could not read object from "
+					+ fname + "!");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			Log.e(this.getClass().getName(),
+					"Could not find class: " + e.getMessage()
+							+ " reading from " + fname + "!");
+			e.printStackTrace();
+		}
+		return null;
+	}
 
-  public long readFreshness() {
-    String s = readText(FRESHNESS);
-    Log.i("CaratDataStorage", "Read freshness: " +s);
-    if (s != null)
-      return Long.parseLong(s);
-    else
-      return -1;
-  }
+	public void writeText(String thing, String fname) {
+		FileOutputStream fos = getFos(fname);
+		if (fos == null)
+			return;
+		try {
+			DataOutputStream dos = new DataOutputStream(fos);
+			dos.writeUTF(thing);
+			dos.close();
+		} catch (IOException e) {
+			Log.e(this.getClass().getName(), "Could not write text:" + thing
+					+ "!");
+			e.printStackTrace();
+		}
+	}
 
-  public Reports readReports() {
-    Object o = readObject(FILENAME);
-    Log.i("CaratDataStorage", "Read Reports: " +o);
-    if (o != null)
-      return (Reports) o;
-    else
-      return null;
-  }
+	public String readText(String fname) {
+		FileInputStream fin = getFin(fname);
+		if (fin == null)
+			return null;
+		try {
+			DataInputStream din = new DataInputStream(fin);
+			String s = din.readUTF();
+			din.close();
+			return s;
+		} catch (IOException e) {
+			Log.e(this.getClass().getName(), "Could not read text from "
+					+ fname + "!");
+			e.printStackTrace();
+		}
+		return null;
+	}
 
-  /**
-   * @return the freshness
-   */
-  public long getFreshness() {
-    return freshness;
-  }
+	private FileInputStream getFin(String fname) {
+		try {
+			return a.openFileInput(fname);
+		} catch (FileNotFoundException e) {
+			Log.e(this.getClass().getName(), "Could not open carat data file "
+					+ fname + " for reading!");
+			e.printStackTrace();
+			return null;
+		}
+	}
 
-  /**
-   * @return the caratData
-   */
-  public Reports getReports() {
-    return caratData;
-  }
+	private FileOutputStream getFos(String fname) {
+		try {
+			return a.openFileOutput(fname, Context.MODE_PRIVATE);
+		} catch (FileNotFoundException e) {
+			Log.e(this.getClass().getName(), "Could not open carat data file "
+					+ fname + " for writing!");
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public long readFreshness() {
+		String s = readText(FRESHNESS);
+		Log.i("CaratDataStorage", "Read freshness: " + s);
+		if (s != null)
+			return Long.parseLong(s);
+		else
+			return -1;
+	}
+
+	public Reports readReports() {
+		Object o = readObject(FILENAME);
+		Log.i("CaratDataStorage", "Read Reports: " + o);
+		if (o != null)
+			return (Reports) o;
+		else
+			return null;
+	}
+
+	/**
+	 * @return the freshness
+	 */
+	public long getFreshness() {
+		return freshness;
+	}
+
+	/**
+	 * @return the caratData
+	 */
+	public Reports getReports() {
+		return caratData;
+	}
+
+	/**
+	 * @return the bug reports
+	 */
+	public List<HogsBugs> getBugReport() {
+		if (bugData == null)
+			return null;
+		return bugData.getHbList();
+	}
+
+	/**
+	 * @return the hog reports
+	 */
+	public List<HogsBugs> getHogReport() {
+		if (bugData == null)
+			return null;
+		return hogData.getHbList();
+	}
+
+	public void writeBugReport(HogBugReport r) {
+		bugData = r;
+		writeObject(r, BUGFILE);
+
+	}
+
+	public void writeHogReport(HogBugReport r) {
+		hogData = r;
+		writeObject(r, HOGFILE);
+
+	}
 }
