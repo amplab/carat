8914bb8f246d28efd7919ff7cc7f30d70e5c6123
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2012-04-15 10:57:30 -0700

    Revert "Add Sample in the Main Activity!" to remove binary files.
    This reverts commit 29256b57cca63d699d1a5523be29b02cdd4e9656.

--- app/android/gen/edu/berkeley/cs/amplab/carat/android/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package edu.berkeley.cs.amplab.carat.android;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
