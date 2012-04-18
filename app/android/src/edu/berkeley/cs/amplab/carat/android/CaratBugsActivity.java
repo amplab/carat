a24306ad597d7406ae3786b155227f70c1f49fd1
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2012-04-18 10:21:18 -0700

    Added a javascript-based chart to the bugs screen. Need to convert this to a flash-based one for older Android versions.

--- app/android/src/edu/berkeley/cs/amplab/carat/android/CaratBugsActivity.java
+++ app/android/src/edu/berkeley/cs/amplab/carat/android/CaratBugsActivity.java
@@ -2,16 +2,35 @@ package edu.berkeley.cs.amplab.carat.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class CaratBugsActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		TextView textview = new TextView(this);
+		setContentView(R.layout.bugs);
+		WebView webview = (WebView) findViewById(R.id.bugsView);
+		//Fixes the white flash when showing the page for the first time.
+		webview.setBackgroundColor(0);
+		webview.getSettings().setJavaScriptEnabled(true);
+		/*
+		 * getWindow().requestFeature(Window.FEATURE_PROGRESS);
+		 * 
+		 * 
+		 */
+		/*
+		 * To display the amplab_logo, we need to have it stored in assets as
+		 * well. If we don't want to do that, the loadConvoluted method below
+		 * avoids it.
+		 */
+		webview.loadUrl("file:///android_asset/twolinechart.html");
+		webview.setOnTouchListener(SwipeListener.instance);
+		
+		/*TextView textview = new TextView(this);
 		textview.setFocusable(true);
 		textview.setText("This is the Bugs tab");
 		setContentView(textview);
-		textview.getRootView().setOnTouchListener(SwipeListener.instance);
+		textview.getRootView().setOnTouchListener(SwipeListener.instance);*/
 	}
 }
