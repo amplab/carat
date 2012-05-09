68c1ab6efc3848234b1f7e99da290cb64afd79ef
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2012-05-09 21:58:41 +0300

    Protocol improvements. Do not recreate client every time.

--- app/android/src/edu/berkeley/cs/amplab/carat/android/protocol/ProtocolClient.java
+++ app/android/src/edu/berkeley/cs/amplab/carat/android/protocol/ProtocolClient.java
@@ -6,6 +6,7 @@ import java.util.Properties;
 
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.protocol.TProtocolFactory;
 import org.apache.thrift.transport.TSocket;
 
 import android.content.Context;
@@ -20,87 +21,86 @@ import edu.berkeley.cs.amplab.carat.thrift.CaratService;
  * 
  */
 public class ProtocolClient {
-	public static final String SERVER_PROPERTIES = "caratserver.properties";
-	public static int SERVER_PORT = 0;
-	public static String SERVER_ADDRESS = null;
+    public static final String TAG = "ProtocolClient";
+    public static final String SERVER_PROPERTIES = "caratserver.properties";
+    public static int SERVER_PORT = 0;
+    public static String SERVER_ADDRESS = null;
 
-	private static CaratService.Client instance = null;
-	private static TSocket soc = null;
+    private static CaratService.Client instance = null;
+    private static TSocket soc = null;
 
-	/**
-	 * FIXME: this needs to come from a factory, so that connections are not
-	 * kept open unnecessarily, and that they do not become stale, and that we
-	 * handle disconnections gracefully.
-	 * 
-	 * @param c
-	 * @return
-	 */
-	public static CaratService.Client getInstance(Context c) {
-		if (SERVER_ADDRESS == null) {
-			Properties properties = new Properties();
-			try {
-				InputStream raw = c.getAssets().open(SERVER_PROPERTIES);
-				if (raw != null) {
-					properties.load(raw);
-					if (properties.containsKey("PORT"))
-						SERVER_PORT = Integer.parseInt(properties.getProperty(
-								"PORT", "8080"));
-					if (properties.containsKey("ADDRESS"))
-						SERVER_ADDRESS = properties.getProperty("ADDRESS",
-								"server.caratproject.com");
+    /**
+     * FIXME: this needs to come from a factory, so that connections are not
+     * kept open unnecessarily, and that they do not become stale, and that we
+     * handle disconnections gracefully.
+     * 
+     * @param c
+     * @return
+     */
+    public static CaratService.Client getInstance(Context c) {
+        if (SERVER_ADDRESS == null) {
+            Properties properties = new Properties();
+            try {
+                InputStream raw = c.getAssets().open(SERVER_PROPERTIES);
+                if (raw != null) {
+                    properties.load(raw);
+                    if (properties.containsKey("PORT"))
+                        SERVER_PORT = Integer.parseInt(properties.getProperty(
+                                "PORT", "8080"));
+                    if (properties.containsKey("ADDRESS"))
+                        SERVER_ADDRESS = properties.getProperty("ADDRESS",
+                                "server.caratproject.com");
 
-					Log.d("CaratProtocol", "Set address=" + SERVER_ADDRESS
-							+ " port=" + SERVER_PORT);
-				} else
-					Log.e("CaratProtocol",
-							"Could not open server property file!");
-			} catch (IOException e) {
-				Log.e("CaratProtocol", "Could not open server property file: "
-						+ e.toString());
-			}
-		}
+                    Log.d(TAG, "Set address=" + SERVER_ADDRESS + " port="
+                            + SERVER_PORT);
+                } else
+                    Log.e(TAG, "Could not open server property file!");
+            } catch (IOException e) {
+                Log.e(TAG,
+                        "Could not open server property file: " + e.toString());
+            }
+        }
 
-		// Initialize server port and address
-		boolean tryagain = true;
-		while (tryagain) {
-			try {
-				if (soc == null)
-					soc = new TSocket(SERVER_ADDRESS, SERVER_PORT);
-				else
-					soc.close();
-				soc.open();
-				TProtocol p = new TBinaryProtocol(soc, true, true);
-				instance = new CaratService.Client(p);
-				tryagain = false;
-			} catch (Exception e) {
-				Log.e("CaratProtocol", "Could not create instance!");
-				e.printStackTrace();
-				tryagain = false;
-			} catch (Throwable th) {
-				// Try again if get an unknown error.
-				Log.e("CaratProtocol",
-						"Socket error? Creating new soc and protocol.");
-				th.printStackTrace();
-				soc = new TSocket(SERVER_ADDRESS, SERVER_PORT);
-				TProtocol p = new TBinaryProtocol(soc, true, true);
-				instance = new CaratService.Client(p);
-				tryagain = true;
-			}
-		}
-		return instance;
-	}
+        // Initialize server port and address
+        boolean tryagain = true;
+        while (tryagain) {
+            try {
+                if (soc == null){
+                    soc = new TSocket(SERVER_ADDRESS, SERVER_PORT);
+                    TProtocol p = new TBinaryProtocol(soc, true, true);
+                    instance = new CaratService.Client(p);
+                }
+                if (!soc.isOpen())
+                    soc.open();
+                tryagain = false;
+            } catch (Exception e) {
+                Log.e(TAG, "Could not create instance!");
+                e.printStackTrace();
+                tryagain = false;
+            } catch (Throwable th) {
+                // Try again if get an unknown error.
+                Log.e(TAG, "Socket error? Creating new soc and protocol.");
+                th.printStackTrace();
+                soc = new TSocket(SERVER_ADDRESS, SERVER_PORT);
+                TProtocol p = new TBinaryProtocol(soc, true, true);
+                instance = new CaratService.Client(p);
+                tryagain = true;
+            }
+        }
+        return instance;
+    }
 
-	public static void close() {
-		soc.close();
-	}
+    public static void close() {
+        soc.close();
+    }
 
-	/**
-	 * Test program.
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	}
+    /**
+     * Test program.
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+    }
 
 }
