b7abf62208fede06dbf3fa4954e326751c8b0333
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2013-08-30 21:10:46 +0300

    The connecting flag doesn't make sense any more.

--- app/android/src/edu/berkeley/cs/amplab/carat/android/protocol/SampleSender.java
+++ app/android/src/edu/berkeley/cs/amplab/carat/android/protocol/SampleSender.java
@@ -25,126 +25,127 @@ public class SampleSender {
     
     private static final String TAG = "sendSamples";
     
-    private static final String TRY_AGAIN = " will try again in "
-            + (CaratApplication.COMMS_INTERVAL / 1000) + "s.";
+    private static final String TRY_AGAIN = " will try again later.";
+    
+    private static final Object sendLock = new Object();
 
     CaratApplication app = null;
 
     // Prevent instantiation
     private SampleSender(){}
     
-    public static void sendSamples(boolean connecting, CaratApplication app) {
-        Context c = app.getApplicationContext();
-
-        String networkStatus = SamplingLibrary.getNetworkStatus(c);
-        String networkType = SamplingLibrary.getNetworkType(c);
-
-        final SharedPreferences p = PreferenceManager
-                .getDefaultSharedPreferences(c);
-        final boolean useWifiOnly = p.getBoolean(
-                CaratApplication.PREFERENCE_WIFI_ONLY, false);
-
-        boolean connected = (!useWifiOnly && networkStatus == SamplingLibrary.NETWORKSTATUS_CONNECTED)
-                || networkType.equals("WIFI");
-
-        if (connected) {
-            CaratSampleDB db = CaratSampleDB.getInstance(c);
-            int samples = db.countSamples();
-            int successSum = 0;
-            for (int batches = 0; batches < CaratApplication.COMMS_MAX_BATCHES
-                    && batches < samples
-                            / CaratApplication.COMMS_MAX_UPLOAD_BATCH + 1; batches++) {
-                SortedMap<Long, Sample> map = CaratSampleDB.getInstance(c)
-                        .queryOldestSamples(
-                                CaratApplication.COMMS_MAX_UPLOAD_BATCH);
-                if (map.size() > 0) {
-                    CaratApplication.setActionProgress(0, successSum + "/" + samples
-                            +" "+app.getString(R.string.samplesreported), false);
-                    if (app.c != null) {
-                        int tries = 0;
-                        while (tries < 2) {
-                            try {
-                                int success = app.c.uploadSamples(map.values());
-
-                                tries = 2;
-                                // FlurryAgent.logEvent("UploadSamples");
-                                Log.d(TAG, "Uploaded " + success
-                                        + " samples out of " + map.size());
-                                if (success > 0)
-                                    CaratApplication.s.samplesReported(success);
-                                Sample last = map.get(map.lastKey());
-                                Log.d(TAG,
-                                        "Deleting " + success
-                                                + " samples older than "
-                                                + last.getTimestamp());
-                                /*
-                                 * Log.i(TAG, "Sent samples:"); for (Sample k:
-                                 * map.values()){ Log.i(TAG, k.getTimestamp() +
-                                 * " " + k.getBatteryLevel()); }
-                                 */
-                                SortedSet<Long> uploaded = new TreeSet<Long>();
-                                int i = 0;
-                                for (Long s : map.keySet()) {
-                                    if (i < success)
-                                        uploaded.add(s);
-                                    i += 1;
+    public static void sendSamples(CaratApplication app) {
+        synchronized(sendLock){
+            Context c = app.getApplicationContext();
+    
+            String networkStatus = SamplingLibrary.getNetworkStatus(c);
+            String networkType = SamplingLibrary.getNetworkType(c);
+    
+            final SharedPreferences p = PreferenceManager
+                    .getDefaultSharedPreferences(c);
+            final boolean useWifiOnly = p.getBoolean(
+                    CaratApplication.PREFERENCE_WIFI_ONLY, false);
+    
+            boolean connected = (!useWifiOnly && networkStatus == SamplingLibrary.NETWORKSTATUS_CONNECTED)
+                    || networkType.equals("WIFI");
+    
+            if (connected) {
+                CaratSampleDB db = CaratSampleDB.getInstance(c);
+                int samples = db.countSamples();
+                int successSum = 0;
+                for (int batches = 0; batches < CaratApplication.COMMS_MAX_BATCHES
+                        && batches < samples
+                                / CaratApplication.COMMS_MAX_UPLOAD_BATCH + 1; batches++) {
+                    SortedMap<Long, Sample> map = CaratSampleDB.getInstance(c)
+                            .queryOldestSamples(
+                                    CaratApplication.COMMS_MAX_UPLOAD_BATCH);
+                    if (map.size() > 0) {
+                        int progress = (int) (successSum * 1.0 / samples * 100.0);
+                        CaratApplication.setActionProgress(progress, successSum + "/"
+                                + samples +" "+ app.getString(R.string.samplesreported), false);
+                        if (app.c != null) {
+                            int tries = 0;
+                            while (tries < 2) {
+                                try {
+                                    int success = app.c.uploadSamples(map.values());
+    
+                                    tries = 2;
+                                    // FlurryAgent.logEvent("UploadSamples");
+                                    Log.d(TAG, "Uploaded " + success
+                                            + " samples out of " + map.size());
+                                    if (success > 0)
+                                        CaratApplication.s.samplesReported(success);
+                                    Sample last = map.get(map.lastKey());
+                                    Log.d(TAG,
+                                            "Deleting " + success
+                                                    + " samples older than "
+                                                    + last.getTimestamp());
+                                    /*
+                                     * Log.i(TAG, "Sent samples:"); for (Sample k:
+                                     * map.values()){ Log.i(TAG, k.getTimestamp() +
+                                     * " " + k.getBatteryLevel()); }
+                                     */
+                                    SortedSet<Long> uploaded = new TreeSet<Long>();
+                                    int i = 0;
+                                    for (Long s : map.keySet()) {
+                                        if (i < success)
+                                            uploaded.add(s);
+                                        i += 1;
+                                    }
+                                    int deleted = CaratSampleDB.getInstance(c)
+                                            .deleteSamples(uploaded);
+                                    /*
+                                     * .deleteOldestSamples( last.getTimestamp());
+                                     */
+                                    Log.d(TAG, "Deleted " + deleted + " samples.");
+                                    successSum += success;
+                                } catch (Throwable th) {
+                                    // Any sort of malformed response, too short
+                                    // string, etc...
+                                    Log.w(TAG, "Failed to refresh reports: "
+                                            + th
+                                            + (tries < 1 ? "Trying again now"
+                                                    : TRY_AGAIN), th);
+                                    tries++;
                                 }
-                                int deleted = CaratSampleDB.getInstance(c)
-                                        .deleteSamples(uploaded);
-                                /*
-                                 * .deleteOldestSamples( last.getTimestamp());
-                                 */
-                                Log.d(TAG, "Deleted " + deleted + " samples.");
-                                successSum += success;
-                            } catch (Throwable th) {
-                                // Any sort of malformed response, too short
-                                // string, etc...
-                                Log.w(TAG, "Failed to refresh reports: "
-                                        + th
-                                        + (tries < 1 ? "Trying again now"
-                                                : TRY_AGAIN), th);
-                                tries++;
                             }
+                        } else {
+                            Log.w(TAG, "CommunicationManager is not ready yet."
+                                    + TRY_AGAIN);
                         }
                     } else {
-                        Log.w(TAG, "CommunicationManager is not ready yet."
-                                + TRY_AGAIN);
+                        Log.w(TAG, "No samples to send." + TRY_AGAIN);
                     }
-                } else {
-                    Log.w(TAG, "No samples to send." + TRY_AGAIN);
                 }
-                int progress = (int) (successSum * 1.0 / samples * 100.0);
-                CaratApplication.setActionProgress(progress, successSum + "/"
-                        + samples +" "+ app.getString(R.string.samplesreported), false);
+            }/* else if (networkStatus
+                    .equals(SamplingLibrary.NETWORKSTATUS_CONNECTING)) {
+                Log.w(TAG, "Network status: " + networkStatus
+                        + ", trying again in 10s.");
+                connecting = true;
+            } else {
+                Log.w(TAG, "Network status: " + networkStatus + TRY_AGAIN);
+                connecting = false;
             }
-        } else if (networkStatus
-                .equals(SamplingLibrary.NETWORKSTATUS_CONNECTING)) {
-            Log.w(TAG, "Network status: " + networkStatus
-                    + ", trying again in 10s.");
-            connecting = true;
-        } else {
-            Log.w(TAG, "Network status: " + networkStatus + TRY_AGAIN);
-            connecting = false;
-        }
-        if (connecting) {
-            // wait for wifi to come up
-            try {
-                Thread.sleep(CaratApplication.COMMS_WIFI_WAIT);
-            } catch (InterruptedException e1) {
-                // ignore
-            }
-            connecting = false;
-        } else {
-            try {
-                Thread.sleep(CaratApplication.COMMS_INTERVAL);
-            } catch (InterruptedException e) {
+            if (connecting) {
                 // wait for wifi to come up
                 try {
                     Thread.sleep(CaratApplication.COMMS_WIFI_WAIT);
                 } catch (InterruptedException e1) {
                     // ignore
                 }
-            }
+                connecting = false;
+            } else {
+                try {
+                    Thread.sleep(CaratApplication.COMMS_INTERVAL);
+                } catch (InterruptedException e) {
+                    // wait for wifi to come up
+                    try {
+                        Thread.sleep(CaratApplication.COMMS_WIFI_WAIT);
+                    } catch (InterruptedException e1) {
+                        // ignore
+                    }
+                }
+            }*/
         }
     }
 }
