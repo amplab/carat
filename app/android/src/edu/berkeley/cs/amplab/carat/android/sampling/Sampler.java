095ab2f2cd175aa5ebf43992e216a502feed5f11
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2014-03-04 12:11:32 +0200

    Make sampling more reliable by using a background process and a wake lock.

--- app/android/src/edu/berkeley/cs/amplab/carat/android/sampling/Sampler.java
+++ app/android/src/edu/berkeley/cs/amplab/carat/android/sampling/Sampler.java
@@ -1,198 +1,120 @@
 package edu.berkeley.cs.amplab.carat.android.sampling;
 
-import java.util.Date;
 import java.util.List;
 
 import edu.berkeley.cs.amplab.carat.android.CaratApplication;
-import edu.berkeley.cs.amplab.carat.android.storage.CaratSampleDB;
-import edu.berkeley.cs.amplab.carat.thrift.Sample;
-import android.content.BroadcastReceiver;
+import android.support.v4.content.WakefulBroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
 
-public class Sampler extends BroadcastReceiver implements LocationListener{
+public class Sampler extends WakefulBroadcastReceiver implements
+        LocationListener {
 
     private static final String TAG = "Sampler";
-    
+
     private static Sampler instance = null;
-    
-    public static Sampler getInstance(){
+
+    public static Sampler getInstance() {
         if (instance != null)
             return instance;
         else
             return new Sampler();
     }
-    
-	CaratSampleDB ds = null;
-	private SharedPreferences sharedPreferences;
-	private Editor editor;
-	private Context context = null;
-	
-	private double lastBatteryLevel = 0.0;
-	
-	public Sampler(){
-	    Sampler.instance = this;
-	}
-	
-	private void requestLocationUpdates(){
-	    LocationManager lm = (LocationManager) context
+
+    private Context context = null;
+
+    private double lastBatteryLevel = 0;
+    private Location lastKnownLocation = null;
+    private double distance = 0.0;
+
+    public Sampler() {
+        Sampler.instance = this;
+    }
+
+    private void requestLocationUpdates() {
+        LocationManager lm = (LocationManager) context
                 .getSystemService(Context.LOCATION_SERVICE);
-	    lm.removeUpdates(this);
-        List<String> providers = SamplingLibrary.getEnabledLocationProviders(context);
-        if (providers != null){
-            for (String provider: providers){
-            lm.requestLocationUpdates(provider, CaratApplication.FRESHNESS_TIMEOUT, 0, this);
-            //Log.v(TAG, "Location updates requested for " + provider);
+        lm.removeUpdates(this);
+        List<String> providers = SamplingLibrary
+                .getEnabledLocationProviders(context);
+        if (providers != null) {
+            for (String provider : providers) {
+                lm.requestLocationUpdates(provider,
+                        CaratApplication.FRESHNESS_TIMEOUT, 0, this);
+                // Log.v(TAG, "Location updates requested for " + provider);
             }
         }
-	}
-	
-	@Override
-	public void onReceive(Context context, Intent intent) {
-	    /*try{
-            context.unregisterReceiver(this);
-        }catch(IllegalArgumentException e){
-            IntentFilter intentFilter = new IntentFilter();
-            intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
-            context.registerReceiver(getInstance(), intentFilter);
-        }*/
-	    
-	    
-		if (ds == null) {
-		    this.context = context;
-			ds = new CaratSampleDB(context);
-			requestLocationUpdates();
-		}
-		final Context c = context;
-		final Intent i = intent;
-		//Log.i(TAG, "Intent: "+i.getAction() +" ds: " +i.getData());
-		
-		if (i.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-			//NOTE: This is disabled to simplify how Carat behaves. 
-		    sharedPreferences = context.getSharedPreferences("SystemBootTime", Context.MODE_PRIVATE); 
-		            editor = sharedPreferences.edit(); 
-		            editor.putLong("bootTime", new Date().getTime()); 
-		            editor.commit();
-			//onBoot(context);
-		}
-		
-		/* If a package is uninstalled, remove it from our sent-sigs so if it is reinstalled, it will be sent again.
-		 * Also, send the uninstallation flag in the next sample. */
-		if (i.getAction().equals(Intent.ACTION_PACKAGE_REMOVED)) {
-			String uninstalledPackage = i.getDataString();
-			// package:
-			// 012345678
-			uninstalledPackage = uninstalledPackage.substring(8);
-			//Log.i(TAG,  "UNINSTALLED: "+ uninstalledPackage);
-			SharedPreferences p = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
-			p.edit().remove(SamplingLibrary.SIG_SENT_256+uninstalledPackage)
-			.remove(SamplingLibrary.SIG_SENT+uninstalledPackage)
-			.putBoolean(SamplingLibrary.UNINSTALLED+uninstalledPackage, true).commit();
-		}
-		
-		/* 
-		 * Some phones receive the batteryChanged very very often.
-		 * We are interested only in changes of the battery level. 
-		 */ 
-		
-		double bl = SamplingLibrary.getBatteryLevel(context, intent);
-		if (bl <= 0)
-		    return;
-		
-		Sample lastSample = ds.getLastSample(context);
-		
-		if ((lastSample == null && lastBatteryLevel != bl) || (lastSample != null && lastSample.getBatteryLevel() != bl)) {
-		    Log.i(TAG, "Sampling for intent="+i.getAction()+" lastSample=" + (lastSample != null ? lastSample.getBatteryLevel()+"": "null") + " current battery="+bl);
-		    // Take a sample.
-		    getSample(c, i, lastSample);
-		    lastBatteryLevel = bl;
-		}
-	}
-	
-	/**
-	 * Used to start Sampler on reboot even when Carat is not started.
-	 * Not used at the moment to keep Carat simple.
-	 * @param context
-	 */
+    }
 
-	private void onBoot(Context context){
-		// Schedule recurring sampling event:
-		// What to start when the event fires (this is unused at the moment)
-		Intent in = new Intent(context, Sampler.class);
-		in.setAction(CaratApplication.ACTION_CARAT_SAMPLE);
-		// In reality, you would want to have a static variable for the
-		// request code instead of 192837
-		/*PendingIntent sender = PendingIntent.getBroadcast(context, 192837,
-				in, PendingIntent.FLAG_UPDATE_CURRENT);
-        */
-		// Get the AlarmManager service
-		/*AlarmManager am = (AlarmManager) context
-				.getSystemService(Activity.ALARM_SERVICE);
-		// 1 min first, 15 min intervals
-		am.setInexactRepeating(AlarmManager.RTC_WAKEUP,
-				CaratApplication.FIRST_SAMPLE_DELAY_MS,
-				CaratApplication.SAMPLE_INTERVAL_MS, sender);
-		*/
-		IntentFilter intentFilter = new IntentFilter();
-		intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
-		context.registerReceiver(new Sampler(), intentFilter);
-	}
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (this.context == null) {
+            this.context = context;
+            requestLocationUpdates();
+        }
 
-	/**
-	 * Get a Sample and store it in the database. Do not store the first ever samples on a device that have no battery info.
-	 * @param context from onReceive
-	 * @param intent from onReceive
-	 * @return the newly recorded Sample
-	 */
-	private Sample getSample(Context context, Intent intent, Sample lastSample) {
-	    // Update last known location...
-	    if (lastKnownLocation == null)
-	        lastKnownLocation = SamplingLibrary.getLastKnownLocation(context);
-		Sample s = SamplingLibrary.getSample(context, intent,
-				lastSample);
-		// Set distance to current distance value
-		if (s != null){
-		    //Log.v(TAG, "distanceTravelled=" + distance);
-		    s.setDistanceTraveled(distance);
-		    // FIX: Do not use same distance again.
-		    distance = 0;
-		}
+        // Update last known location...
+        if (lastKnownLocation == null)
+            lastKnownLocation = SamplingLibrary.getLastKnownLocation(context);
 
-		// Write to database
-		// But only after first real numbers
-		if (!s.getBatteryState().equals("Unknown") && s.getBatteryLevel() >= 0) {
-			long id = ds.putSample(s);
-			//Log.d(TAG, "Took sample " + id + " for " + intent.getAction());
-			//FlurryAgent.logEvent(intent.getAction());
-			/*Toast.makeText(context,
-					"Took sample " + id + " for " + intent.getAction(),
-					Toast.LENGTH_LONG).show();*/
-		}
-		return s;
-	}
-	
-	private Location lastKnownLocation = null;
-	private double distance = 0.0;
+        Intent service = new Intent(context, SamplerService.class);
+        service.putExtra("OriginalAction", intent.getAction());
+        service.fillIn(intent, 0);
+        service.putExtra("lastBatteryLevel", lastBatteryLevel);
+        service.putExtra("distance", distance);
+        startWakefulService(context, service);
+    }
+
+    /**
+     * Used to start Sampler on reboot even when Carat is not started. Not used
+     * at the moment to keep Carat simple.
+     * 
+     * @param context
+     */
+
+    private void onBoot(Context context) {
+        // Schedule recurring sampling event:
+        // What to start when the event fires (this is unused at the moment)
+        Intent in = new Intent(context, Sampler.class);
+        in.setAction(CaratApplication.ACTION_CARAT_SAMPLE);
+        // In reality, you would want to have a static variable for the
+        // request code instead of 192837
+        /*
+         * PendingIntent sender = PendingIntent.getBroadcast(context, 192837,
+         * in, PendingIntent.FLAG_UPDATE_CURRENT);
+         */
+        // Get the AlarmManager service
+        /*
+         * AlarmManager am = (AlarmManager) context
+         * .getSystemService(Activity.ALARM_SERVICE); // 1 min first, 15 min
+         * intervals am.setInexactRepeating(AlarmManager.RTC_WAKEUP,
+         * CaratApplication.FIRST_SAMPLE_DELAY_MS,
+         * CaratApplication.SAMPLE_INTERVAL_MS, sender);
+         */
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        context.registerReceiver(new Sampler(), intentFilter);
+    }
 
     @Override
     public void onLocationChanged(Location location) {
         if (lastKnownLocation != null && location != null) {
             distance = lastKnownLocation.distanceTo(location);
             /*
-            HashMap<String, Double> m = new HashMap<String, Double>();
-            m.put("distanceTraveled", distance);*/
-            //FlurryAgent.logEvent("LocationChanged", m);
-            /*CharSequence text = "Location change with distance = " + distance;
-            Toast.makeText(context, text, Toast.LENGTH_LONG).show();*/
+             * HashMap<String, Double> m = new HashMap<String, Double>();
+             * m.put("distanceTraveled", distance);
+             */
+            // FlurryAgent.logEvent("LocationChanged", m);
+            /*
+             * CharSequence text = "Location change with distance = " +
+             * distance; Toast.makeText(context, text,
+             * Toast.LENGTH_LONG).show();
+             */
         }
         lastKnownLocation = location;
     }
---
+++ app/android/src/edu/berkeley/cs/amplab/carat/android/sampling/SamplerService.java
@@ -0,0 +1,138 @@
+package edu.berkeley.cs.amplab.carat.android.sampling;
+
+import java.util.Date;
+
+import edu.berkeley.cs.amplab.carat.android.storage.CaratSampleDB;
+import edu.berkeley.cs.amplab.carat.thrift.Sample;
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.PowerManager;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+public class SamplerService extends IntentService {
+    
+    private static final String TAG = "SamplerService";
+    
+    public SamplerService() {
+        super(TAG);
+    }
+    
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        // At this point SimpleWakefulReceiver is still holding a wake lock
+        // for us.  We can do whatever we need to here and then tell it that
+        // it can release the wakelock.  This sample just does some slow work,
+        // but more complicated implementations could take their own wake
+        // lock here before releasing the receiver's.
+        //
+        // Note that when using this approach you should be aware that if your
+        // service gets killed and restarted while in the middle of such work
+        // (so the Intent gets re-delivered to perform the work again), it will
+        // at that point no longer be holding a wake lock since we are depending
+        // on SimpleWakefulReceiver to that for us.  If this is a concern, you can
+        // acquire a separate wake lock here.
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
+        wl.acquire();
+        
+        Context context = getApplicationContext();
+        
+        String action = intent.getStringExtra("OriginalAction");
+        Log.i(TAG, "Original intent: " +action);
+        if (action != null){
+        double lastBatteryLevel = intent.getDoubleExtra("lastBatteryLevel", 0.0);
+        double distance = intent.getDoubleExtra("distance", 0.0);
+
+        if (action.equals(Intent.ACTION_BOOT_COMPLETED)) {
+            // NOTE: This is disabled to simplify how Carat behaves.
+            SharedPreferences p = context.getSharedPreferences("SystemBootTime",
+                    Context.MODE_PRIVATE);
+            Editor editor = p.edit();
+            editor.putLong("bootTime", new Date().getTime());
+            editor.commit();
+            // onBoot(context);
+        }
+
+        /*
+         * If a package is uninstalled, remove it from our sent-sigs so if it is
+         * reinstalled, it will be sent again. Also, send the uninstallation
+         * flag in the next sample.
+         */
+        if (action.equals(Intent.ACTION_PACKAGE_REMOVED)) {
+            String uninstalledPackage = intent.getDataString();
+            // package:
+            // 012345678
+            uninstalledPackage = uninstalledPackage.substring(8);
+            // Log.i(TAG, "UNINSTALLED: "+ uninstalledPackage);
+            SharedPreferences p = PreferenceManager
+                    .getDefaultSharedPreferences(context
+                            .getApplicationContext());
+            p.edit()
+                    .remove(SamplingLibrary.SIG_SENT_256 + uninstalledPackage)
+                    .remove(SamplingLibrary.SIG_SENT + uninstalledPackage)
+                    .putBoolean(
+                            SamplingLibrary.UNINSTALLED + uninstalledPackage,
+                            true).commit();
+        }
+        
+        CaratSampleDB ds = CaratSampleDB.getInstance(context);
+        
+        /*
+         * Some phones receive the batteryChanged very very often. We are
+         * interested only in changes of the battery level.
+         */
+
+        double bl = SamplingLibrary.getBatteryLevel(context, intent);
+        if (bl <= 0){
+            wl.release();
+            return;
+        }
+        
+        Sample lastSample = ds.getLastSample(context);
+        
+        if ((lastSample == null && lastBatteryLevel != bl) || (lastSample != null && lastSample.getBatteryLevel() != bl)) {
+            //Log.i(TAG, "Sampling for intent="+i.getAction()+" lastSample=" + (lastSample != null ? lastSample.getBatteryLevel()+"": "null") + " current battery="+bl);
+            // Take a sample.
+            getSample(ds, context, intent, lastSample, distance);
+            lastBatteryLevel = bl;
+        }
+        
+        }
+        wl.release();
+        Sampler.completeWakefulIntent(intent);
+    }
+
+    /**
+     * Get a Sample and store it in the database. Do not store the first ever samples on a device that have no battery info.
+     * @param context from onReceive
+     * @param intent from onReceive
+     * @return the newly recorded Sample
+     */
+    private Sample getSample(CaratSampleDB ds, Context context, Intent intent, Sample lastSample, double distance) {
+        Sample s = SamplingLibrary.getSample(context, intent,
+                lastSample);
+        // Set distance to current distance value
+        if (s != null){
+            //Log.v(TAG, "distanceTravelled=" + distance);
+            s.setDistanceTraveled(distance);
+            // FIX: Do not use same distance again.
+            distance = 0;
+        }
+
+        // Write to database
+        // But only after first real numbers
+        if (!s.getBatteryState().equals("Unknown") && s.getBatteryLevel() >= 0) {
+            long id = ds.putSample(s);
+            //Log.d(TAG, "Took sample " + id + " for " + intent.getAction());
+            //FlurryAgent.logEvent(intent.getAction());
+            /*Toast.makeText(context,
+                    "Took sample " + id + " for " + intent.getAction(),
+                    Toast.LENGTH_LONG).show();*/
+        }
+        return s;
+    }
+}
