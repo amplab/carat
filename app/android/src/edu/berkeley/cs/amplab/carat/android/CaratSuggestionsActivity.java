889ab2e77be6ce052fd3b946607ab3ffb07a1644
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2014-03-24 16:26:46 +0200

    Cleaner tracking.

--- app/android/src/edu/berkeley/cs/amplab/carat/android/CaratSuggestionsActivity.java
+++ app/android/src/edu/berkeley/cs/amplab/carat/android/CaratSuggestionsActivity.java
@@ -60,13 +60,12 @@ public class CaratSuggestionsActivity extends BaseVFActivity {
         final ListView lv = (ListView) findViewById(android.R.id.list);
         lv.setCacheColorHint(0);
 
-        //initKillView();
-        //initCollectDataView();
+        // initKillView();
+        // initCollectDataView();
 
         lv.setOnItemClickListener(new OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> a, View v, int position,
-                    long id) {
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
                 Object o = lv.getItemAtPosition(position);
                 SimpleHogBug fullObject = (SimpleHogBug) o;
                 final String raw = fullObject.getAppName();
@@ -93,23 +92,18 @@ public class CaratSuggestionsActivity extends BaseVFActivity {
                     GoToDisplayScreen();
                 else if (raw.equals(getString(R.string.disableautomaticsync)))
                     GoToSyncScreen();
-                else if (raw.equals(getString(R.string.helpcarat))){
+                else if (raw.equals(getString(R.string.helpcarat))) {
                     initCollectDataView();
                     switchView(collectDataView);
-                }else if (raw.equals(getString(R.string.questionnaire))){
+                } else if (raw.equals(getString(R.string.questionnaire))) {
                     openQuestionnaire();
                 } else {
                     initKillView();
-                    ImageView icon = (ImageView) killView
-                            .findViewById(R.id.suggestion_app_icon);
-                    TextView txtName = (TextView) killView
-                            .findViewById(R.id.actionName);
-                    TextView txtType = (TextView) killView
-                            .findViewById(R.id.suggestion_type);
-                    final TextView txtBenefit = (TextView) killView
-                            .findViewById(R.id.expectedBenefit);
-                    final Button killButton = (Button) killView
-                            .findViewById(R.id.killButton);
+                    ImageView icon = (ImageView) killView.findViewById(R.id.suggestion_app_icon);
+                    TextView txtName = (TextView) killView.findViewById(R.id.actionName);
+                    TextView txtType = (TextView) killView.findViewById(R.id.suggestion_type);
+                    final TextView txtBenefit = (TextView) killView.findViewById(R.id.expectedBenefit);
+                    final Button killButton = (Button) killView.findViewById(R.id.killButton);
 
                     final String label = CaratApplication.labelForApp(c, raw);
 
@@ -118,8 +112,7 @@ public class CaratSuggestionsActivity extends BaseVFActivity {
                     Type type = fullObject.getType();
                     if (type == Type.BUG || type == Type.HOG) {
                         txtName.setText(label);
-                        final PackageInfo pak = SamplingLibrary
-                                .getPackageInfo(c, raw);
+                        final PackageInfo pak = SamplingLibrary.getPackageInfo(c, raw);
                         String ver = "";
                         if (pak != null)
                             ver = pak.versionName;
@@ -131,22 +124,24 @@ public class CaratSuggestionsActivity extends BaseVFActivity {
                         killButton.setOnClickListener(new OnClickListener() {
                             @Override
                             public void onClick(View arg0) {
-                            	HashMap<String, String> options = new HashMap<String, String>();
-                            	SharedPreferences p = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                                String uuId = p.getString(CaratApplication.REGISTERED_UUID, "UNKNOWN");
-                                options.put(ClickTracking.USERNAME, uuId);
-                                options.put(ClickTracking.NAME, "killbutton");
-                                if (pak != null) {
-                                	options.put("app", pak.packageName);
-                                	options.put("version", pak.versionName);
-                                	options.put("versionCode", pak.versionCode+"");
-                                	options.put("label", label);
+                                /* Killbutton clicked. Track click: */
+
+                                SharedPreferences p = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                                if (p != null) {
+                                    String uuId = p.getString(CaratApplication.REGISTERED_UUID, "UNKNOWN");
+                                    HashMap<String, String> options = new HashMap<String, String>();
+                                    if (pak != null) {
+                                        options.put("app", pak.packageName);
+                                        options.put("version", pak.versionName);
+                                        options.put("versionCode", pak.versionCode + "");
+                                        options.put("label", label);
+                                    }
+                                    options.put("benefit", txtBenefit.getText().toString().replace('\u00B1', '+'));
+                                    ClickTracking.track(uuId, "killbutton", options);
                                 }
-                                options.put("benefit",txtBenefit.getText().toString().replace('\u00B1', '+'));
-                                ClickTracking.track(options);
+                                /* Change kill button text and make it unclickable until screen is exited */
                                 killButton.setEnabled(false);
-                                killButton.setText(s + " "
-                                        + getString(R.string.killed));
+                                killButton.setText(s + " " + getString(R.string.killed));
                                 SamplingLibrary.killApp(c, raw, label);
                                 // onBackPressed();
                             }
@@ -155,25 +150,18 @@ public class CaratSuggestionsActivity extends BaseVFActivity {
                         txtName.setText(label);
                         killButton.setText(label);
                     }
-                    if (fullObject.getType() == Type.OTHER){
+                    if (fullObject.getType() == Type.OTHER) {
                         // Already translated
                         txtType.setText(fullObject.getAppPriority());
-                    }else
-                    txtType.setText(CaratApplication
-                            .translatedPriority(fullObject.getAppPriority()));
+                    } else
+                        txtType.setText(CaratApplication.translatedPriority(fullObject.getAppPriority()));
 
                     /*
-                     * if (raw.equals("Disable bluetooth")) { double
-                     * benefitOther = PowerProfileHelper. bluetoothBenefit(c);
-                     * hours = (int) (benefitOther); min = (int) (benefitOther *
-                     * 60); min -= hours * 60; } else if
-                     * (raw.equals("Disable Wifi")) { double benefitOther =
-                     * PowerProfileHelper.wifiBenefit(c); hours = (int)
-                     * (benefitOther); min = (int) (benefitOther * 60); min -=
-                     * hours * 60; } else if (raw.equals("Dim the Screen")) {
-                     * double benefitOther = PowerProfileHelper.
-                     * screenBrightnessBenefit(c); hours = (int) (benefitOther);
-                     * min = (int) (benefitOther * 60); min -= hours * 60; }
+                     * if (raw.equals("Disable bluetooth")) { double benefitOther = PowerProfileHelper. bluetoothBenefit(c); hours = (int)
+                     * (benefitOther); min = (int) (benefitOther * 60); min -= hours * 60; } else if (raw.equals("Disable Wifi")) { double
+                     * benefitOther = PowerProfileHelper.wifiBenefit(c); hours = (int) (benefitOther); min = (int) (benefitOther * 60); min
+                     * -= hours * 60; } else if (raw.equals("Dim the Screen")) { double benefitOther = PowerProfileHelper.
+                     * screenBrightnessBenefit(c); hours = (int) (benefitOther); min = (int) (benefitOther * 60); min -= hours * 60; }
                      */
 
                     txtBenefit.setText(fullObject.textBenefit());
@@ -189,8 +177,7 @@ public class CaratSuggestionsActivity extends BaseVFActivity {
         if (o != null) {
             CaratSuggestionsActivity previous = (CaratSuggestionsActivity) o;
             viewIndex = previous.viewIndex;
-            if (previous.killView != null
-                    && previous.killView == previous.vf.getChildAt(viewIndex)) {
+            if (previous.killView != null && previous.killView == previous.vf.getChildAt(viewIndex)) {
                 restoreKillView(previous.killView);
             }
         }
@@ -200,74 +187,63 @@ public class CaratSuggestionsActivity extends BaseVFActivity {
         else
             vf.setDisplayedChild(viewIndex);
     }
-    
+
     private void restoreKillView(View previous) {
         initKillView();
         ImageView icon = (ImageView) killView.findViewById(R.id.suggestion_app_icon);
 
-        icon.setImageDrawable(((ImageView) previous
-                .findViewById(R.id.suggestion_app_icon)).getDrawable());
+        icon.setImageDrawable(((ImageView) previous.findViewById(R.id.suggestion_app_icon)).getDrawable());
         TextView txtName = (TextView) killView.findViewById(R.id.actionName);
-        txtName.setText(((TextView) previous.findViewById(R.id.actionName))
-                .getText());
-        TextView txtType = (TextView) killView
-                .findViewById(R.id.suggestion_type);
-        txtType.setText(((TextView) previous.findViewById(R.id.suggestion_type))
-                .getText());
-        TextView txtBenefit = (TextView) killView
-                .findViewById(R.id.expectedBenefit);
-        txtBenefit.setText(((TextView) previous
-                .findViewById(R.id.expectedBenefit)).getText());
+        txtName.setText(((TextView) previous.findViewById(R.id.actionName)).getText());
+        TextView txtType = (TextView) killView.findViewById(R.id.suggestion_type);
+        txtType.setText(((TextView) previous.findViewById(R.id.suggestion_type)).getText());
+        TextView txtBenefit = (TextView) killView.findViewById(R.id.expectedBenefit);
+        txtBenefit.setText(((TextView) previous.findViewById(R.id.expectedBenefit)).getText());
         Button killButton = (Button) killView.findViewById(R.id.killButton);
-        killButton.setText(((Button) previous.findViewById(R.id.killButton))
-                .getText());
+        killButton.setText(((Button) previous.findViewById(R.id.killButton)).getText());
     }
 
     private void initKillView() {
-        if (killView == null){
-        LayoutInflater inflater = (LayoutInflater) getApplicationContext()
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View killPage = inflater.inflate(R.layout.killlayout, null);
-        LocalizedWebView webview = (LocalizedWebView) killPage.findViewById(R.id.killView);
-        String osVer = SamplingLibrary.getOsVersion();
-        // FIXME: KLUDGE. Should be smarter with the version number.
-        if (osVer.startsWith("2."))
-            webview.loadUrl("file:///android_asset/killapp-2.2.html");
-        else
-            webview.loadUrl("file:///android_asset/killapp.html");
-        // FIXME: This does not work with the embedded WebView (randomly goes back); no idea why
-        killPage.setOnTouchListener(new FlipperBackListener(this, vf, baseViewIndex, true));
-        webview.setOnTouchListener(new FlipperBackListener(this, vf, vf
-                .indexOfChild(findViewById(android.R.id.list)), false));
-        Button AppManagerButton = (Button) killPage.findViewById(R.id.appManager);
-        AppManagerButton.setOnClickListener(new OnClickListener(){
-            @Override
-            public void onClick(View arg0) {
-                GoToAppScreen();
-            } 
-        });
-        killView = killPage;
-        vf.addView(killView);
+        if (killView == null) {
+            LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View killPage = inflater.inflate(R.layout.killlayout, null);
+            LocalizedWebView webview = (LocalizedWebView) killPage.findViewById(R.id.killView);
+            String osVer = SamplingLibrary.getOsVersion();
+            // FIXME: KLUDGE. Should be smarter with the version number.
+            if (osVer.startsWith("2."))
+                webview.loadUrl("file:///android_asset/killapp-2.2.html");
+            else
+                webview.loadUrl("file:///android_asset/killapp.html");
+            // FIXME: This does not work with the embedded WebView (randomly goes back); no idea why
+            killPage.setOnTouchListener(new FlipperBackListener(this, vf, baseViewIndex, true));
+            webview.setOnTouchListener(new FlipperBackListener(this, vf, vf.indexOfChild(findViewById(android.R.id.list)), false));
+            Button AppManagerButton = (Button) killPage.findViewById(R.id.appManager);
+            AppManagerButton.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View arg0) {
+                    GoToAppScreen();
+                }
+            });
+            killView = killPage;
+            vf.addView(killView);
         }
     }
-    
+
     private void initCollectDataView() {
-        if (webview == null){
-        LayoutInflater inflater = (LayoutInflater) getApplicationContext()
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View collectPage = inflater.inflate(R.layout.collectdata, null);
-        webview = (LocalizedWebView) collectPage.findViewById(R.id.collectDataView);
-        webview.loadUrl("file:///android_asset/collectdata.html");
-        webview.setOnTouchListener(new FlipperBackListener(this, vf, baseViewIndex));
-        vf.addView(webview);
+        if (webview == null) {
+            LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View collectPage = inflater.inflate(R.layout.collectdata, null);
+            webview = (LocalizedWebView) collectPage.findViewById(R.id.collectDataView);
+            webview.loadUrl("file:///android_asset/collectdata.html");
+            webview.setOnTouchListener(new FlipperBackListener(this, vf, baseViewIndex));
+            vf.addView(webview);
         }
     }
 
     private void initUpgradeOsView() {
         LocalizedWebView webview = (LocalizedWebView) findViewById(R.id.upgradeOsView);
         webview.loadUrl("file:///android_asset/upgradeos.html");
-        webview.setOnTouchListener(new FlipperBackListener(this, vf, vf
-                .indexOfChild(findViewById(android.R.id.list))));
+        webview.setOnTouchListener(new FlipperBackListener(this, vf, vf.indexOfChild(findViewById(android.R.id.list))));
     }
 
     /* Show the bluetooth setting */
@@ -288,16 +264,14 @@ public class CaratSuggestionsActivity extends BaseVFActivity {
     }
 
     /*
-     * Show the sound setting including phone ringer mode, vibration mode,
-     * haptic feedback setting and other sound options
+     * Show the sound setting including phone ringer mode, vibration mode, haptic feedback setting and other sound options
      */
     public void GoToSoundScreen() {
         safeStart(android.provider.Settings.ACTION_SOUND_SETTINGS, getString(R.string.soundsettings));
     }
 
     /*
-     * Show the location service setting including configuring gps provider,
-     * network provider
+     * Show the location service setting including configuring gps provider, network provider
      */
     public void GoToLocSevScreen() {
         safeStart(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS, getString(R.string.locationsettings));
@@ -309,8 +283,7 @@ public class CaratSuggestionsActivity extends BaseVFActivity {
     }
 
     /*
-     * Show the mobile network setting including configuring 3G/2G, network
-     * operators
+     * Show the mobile network setting including configuring 3G/2G, network operators
      */
     public void GoToMobileNetworkScreen() {
         safeStart(android.provider.Settings.ACTION_DATA_ROAMING_SETTINGS, getString(R.string.mobilenetworksettings));
@@ -329,18 +302,17 @@ public class CaratSuggestionsActivity extends BaseVFActivity {
         } catch (Throwable th) {
             Log.e(TAG, "Could not start activity: " + intent, th);
             if (thing != null) {
-                Toast t = Toast.makeText(getApplicationContext(), getString(R.string.opening)
-                        + thing + getString(R.string.notsupported),
+                Toast t = Toast.makeText(getApplicationContext(), getString(R.string.opening) + thing + getString(R.string.notsupported),
                         Toast.LENGTH_SHORT);
                 t.show();
             }
         }
     }
-    
+
     /**
      * Open a Carat-related questionnaire.
      */
-    public void openQuestionnaire(){
+    public void openQuestionnaire() {
         SharedPreferences p = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         String caratId = Uri.encode(p.getString(CaratApplication.REGISTERED_UUID, ""));
         String os = Uri.encode(SamplingLibrary.getOsVersion());
@@ -368,8 +340,7 @@ public class CaratSuggestionsActivity extends BaseVFActivity {
     public void refresh() {
         CaratApplication app = (CaratApplication) getApplication();
         final ListView lv = (ListView) findViewById(android.R.id.list);
-        lv.setAdapter(new HogBugSuggestionsAdapter(app, CaratApplication.s
-                .getHogReport(), CaratApplication.s.getBugReport()));
+        lv.setAdapter(new HogBugSuggestionsAdapter(app, CaratApplication.s.getHogReport(), CaratApplication.s.getBugReport()));
         emptyCheck(lv);
     }
 
@@ -384,7 +355,9 @@ public class CaratSuggestionsActivity extends BaseVFActivity {
         }
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see android.app.Activity#onRetainNonConfigurationInstance()
      */
     @Override
@@ -395,13 +368,11 @@ public class CaratSuggestionsActivity extends BaseVFActivity {
     /*
      * (non-Javadoc)
      * 
-     * @see
-     * edu.berkeley.cs.amplab.carat.android.ui.BaseVFActivity#onBackPressed()
+     * @see edu.berkeley.cs.amplab.carat.android.ui.BaseVFActivity#onBackPressed()
      */
     @Override
     public void onBackPressed() {
-        if (vf.getDisplayedChild() != baseViewIndex
-                && vf.getDisplayedChild() != emptyIndex) {
+        if (vf.getDisplayedChild() != baseViewIndex && vf.getDisplayedChild() != emptyIndex) {
             SamplingLibrary.resetRunningProcessInfo();
             refresh();
             vf.setOutAnimation(CaratMainActivity.outtoRight);
