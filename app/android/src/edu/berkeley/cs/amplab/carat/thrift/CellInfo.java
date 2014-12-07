63bc3fba0d90fb1098debc2b980dc843e0e5cdc4
Javad Sadeqzadeh <sadeqzadeh@gmail.com>
2014-12-07 02:35:59 +0200

    added the two non-thrift generated source files

---
+++ app/android/src/edu/berkeley/cs/amplab/carat/thrift/CellInfo.java
@@ -0,0 +1,9 @@
+package edu.berkeley.cs.amplab.carat.thrift;
+
+public class CellInfo {
+    public int MCC = 0;
+    public int MNC = 0;
+    public int LAC = 0;
+    public int CID = 0;
+    public String radioType = null;
+}
