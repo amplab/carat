9c8d8aaf8ef05e7709e2edafe200d445e2c1c897
Javad Sadeqzadeh <sadeqzadeh@gmail.com>
2014-12-07 02:32:42 +0200

    regenerated source files after changing the Thrift definition file

--- app/android/src/edu/berkeley/cs/amplab/carat/thrift/CallMonth.java
+++
@@ -1,9 +0,0 @@
-package edu.berkeley.cs.amplab.carat.thrift;
-
-public class CallMonth {
-        public int tolCallInNum=0;
-        public int tolCallOutNum=0;
-        public int tolMissedCallNum=0;
-        public long tolCallInDur=0;
-        public long tolCallOutDur=0;
-}
--- app/android/src/edu/berkeley/cs/amplab/carat/thrift/CellInfo.java
+++
@@ -1,9 +0,0 @@
-package edu.berkeley.cs.amplab.carat.thrift;
-
-public class CellInfo {
-    public int MCC = 0;
-    public int MNC = 0;
-    public int LAC = 0;
-    public int CID = 0;
-    public String radioType = null;
-}
